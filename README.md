65536779eb5de8ba1607cffb214c02417a473719
Mike Ludemann <mike-ludemann@hotmail.de>
2017-11-09 15:46:32 +0100

    First Commit with all data

---
+++ README.md
@@ -0,0 +1,14 @@
+# countdown_timer
+
+Set date and/or time format for a countdown of time
+
+## Example
+
+```sh
+
+Element must be an 'ID'  
+
+countdown_timer("test", "01/01/2017", "12:00:00");
+
+countdown_timer("test", "01/01/2017");
+```
---
+++ countdown_timer.js
@@ -0,0 +1,56 @@
+function countdown_timer(element, date, time){
+	'use strict';
+    
+	var setCountdown;
+    
+    if (time == null) {
+    
+    	setCountdown = new Date(date).getTime();
+    
+    } else if (date != null && time != null) {
+        	
+	    setCountdown = new Date(date + " " + time).getTime();
+    
+    } else {
+    
+    	console.log("Date and/or time format is wrong!");
+    
+    }
+    
+	var x = setInterval(function() {
+
+      var now = new Date().getTime();
+
+      var distance = setCountdown - now;
+
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+      var daysText,
+      	  hoursText,
+          minutesText,
+          secondsText;
+	  
+      if (days == 1) { daysText = " day " } else { daysText = " days " }
+      if (hours == 1) { hoursText = " hour " } else { hoursText = " hours " }
+      if (minutes == 1) { minutesText = " minute " } else { minutesText = " minutes " }
+      if (seconds == 1) { secondsText = " second " } else { secondsText = " seconds " }
+      
+      var elementExist = document.getElementById(element);
+
+      if (distance < 0) {
+
+          clearInterval(x);
+          elementExist.innerHTML = "EXPIRED";
+
+      } else {
+
+          elementExist.innerHTML = days + daysText + hours + hoursText + minutes + minutesText + seconds + secondsText;
+
+      }
+      
+  	}, 1000);
+
+}
