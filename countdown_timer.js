6ffe875c76240b38603ea7d567b817678f41eb70
Mike Ludemann <mike-ludemann@hotmail.de>
2017-11-15 21:30:35 +0100

    Modify files

--- countdown_timer.js
+++ countdown_timer.js
@@ -1,56 +1,58 @@
 function countdown_timer(element, date, time){
-	'use strict';
     
-	var setCountdown;
-    
+    'use strict';
+
+    var setCountdown;
+
     if (time == null) {
-    
-    	setCountdown = new Date(date).getTime();
-    
+
+        setCountdown = new Date(date).getTime();
+
     } else if (date != null && time != null) {
-        	
-	    setCountdown = new Date(date + " " + time).getTime();
-    
+
+        setCountdown = new Date(date + " " + time).getTime();
+
     } else {
-    
-    	console.log("Date and/or time format is wrong!");
-    
+
+        console.log("Date and/or time format is wrong!");
+
     }
-    
-	var x = setInterval(function() {
 
-      var now = new Date().getTime();
+    var x = setInterval(function() {
 
-      var distance = setCountdown - now;
+        var now = new Date().getTime();
 
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      
-      var daysText,
-      	  hoursText,
-          minutesText,
-          secondsText;
-	  
-      if (days == 1) { daysText = " day " } else { daysText = " days " }
-      if (hours == 1) { hoursText = " hour " } else { hoursText = " hours " }
-      if (minutes == 1) { minutesText = " minute " } else { minutesText = " minutes " }
-      if (seconds == 1) { secondsText = " second " } else { secondsText = " seconds " }
-      
-      var elementExist = document.getElementById(element);
+        var distance = setCountdown - now;
 
-      if (distance < 0) {
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+            seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-          clearInterval(x);
-          elementExist.innerHTML = "EXPIRED";
+        var daysText,
+            hoursText,
+            minutesText,
+            secondsText;
 
-      } else {
+        if (days == 1) { daysText = " day " } else { daysText = " days " }
+        if (hours == 1) { hoursText = " hour " } else { hoursText = " hours " }
+        if (minutes == 1) { minutesText = " minute " } else { minutesText = " minutes " }
+        if (seconds == 1) { secondsText = " second " } else { secondsText = " seconds " }
 
-          elementExist.innerHTML = days + daysText + hours + hoursText + minutes + minutesText + seconds + secondsText;
+        var elementExist = document.getElementById(element);
 
-      }
-      
-  	}, 1000);
+        if (distance < 0) {
+
+            clearInterval(x);
+            
+            elementExist.innerHTML = "EXPIRED";
+
+        } else {
+
+            elementExist.innerHTML = days + daysText + hours + hoursText + minutes + minutesText + seconds + secondsText;
+
+        }
+
+    }, 1000);
 
 }
